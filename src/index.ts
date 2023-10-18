ae7cde8f58d1289cb8c0a83a34f6bc65f0d913c9
gnuxie <Gnuxie@protonmail.com>
2023-10-18 14:45:17 +0100

    Update src/index.ts to reflect recent additions to library.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+// For the love of god keep this in alphabetical order please.
 export * from './Interface/Action';
 export * from './Interface/ActionException';
 export * from './Interface/InternedInstanceFactory';
 export * from './Interface/PersistentData';
+export * from './Interface/Value';
 
 export * from './Logging/Logger';
 
@@ -10,14 +12,16 @@ export * from './MatrixTypes/Events';
 export * from './MatrixTypes/MatrixEntity';
 export * from './MatrixTypes/MatrixGlob';
 export * from './MatrixTypes/MatrixRoomReference';
+export * from './MatrixTypes/MembershipEvent';
 export * from './MatrixTypes/Permalinks';
 export * from './MatrixTypes/PolicyEvents';
 export * from './MatrixTypes/PowerLevels';
+export * from './MatrixTypes/StringlyTypedMatrix';
 
-export * from './PolicyList/PolicyRoomEditor';
-export * from './PolicyList/PolicyRoomManger';
 export * from './PolicyList/PolicyListRevision';
 export * from './PolicyList/PolicyListRevisionIssuer';
+export * from './PolicyList/PolicyRoomEditor';
+export * from './PolicyList/PolicyRoomManger';
 export * from './PolicyList/PolicyRule';
 export * from './PolicyList/PolicyRuleChange';
 export * from './PolicyList/Revision';
@@ -26,5 +30,16 @@ export * from './PolicyList/StandardPolicyRoomRevision';
 export * from './PolicyList/StandardPolicyRoomRevisionIssuer';
 
 export * from './Protection/Consequence';
+export * from './Protection/DirectPropagationPolicyListRevisionIssuer';
+export * from './Protection/PolicyListRevisionIssuerConfig';
 export * from './Protection/ProtectedRoomsSet';
 export * from './Protection/Protection';
+export * from './Protection/ProtectionsConfig';
+
+export * from './StateTracking/EventBatch';
+export * from './StateTracking/MembershipChange';
+export * from './StateTracking/MembershipRevision';
+export * from './StateTracking/MembershipRevisionIssuer';
+export * from './StateTracking/RoomMembershipManager';
+export * from './StateTracking/StandardRoomMembershipRevision';
+export * from './StateTracking/StandardRoomMembershipRevisionIssuer';
