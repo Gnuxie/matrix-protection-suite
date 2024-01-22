084d1623fb75a689fb2d1e7760651a2552332d50
gnuxie <Gnuxie@protonmail.com>
2024-01-22 12:29:22 +0000

    Basic Unit test for MembershipBanSynchronisationProtection.

--- src/Protection/StandardProtections/MemberBanSynchronisation.test.ts
+++ src/Protection/StandardProtections/MemberBanSynchronisation.test.ts
@@ -1,10 +1,37 @@
 import { BasicConsequenceProvider } from '../Consequence/Consequence';
 import { createMock } from 'ts-auto-mock';
-import { ProtectedRoomsSet } from '../ProtectedRoomsSet';
 import { findProtection } from '../Protection';
 import './MemberBanSynchronisation';
+import { isError, isOk } from '../../Interface/Action';
+import { randomRoomID } from '../../TestUtilities/EventGeneration';
+import { StringUserID } from '../../MatrixTypes/StringlyTypedMatrix';
+import { describeProtectedRoomsSet } from '../../StateTracking/DeclareRoomState';
+import { Membership } from '../../StateTracking/MembershipChange';
+import waitForExpect from 'wait-for-expect';
+import { PolicyRuleType } from '../../MatrixTypes/PolicyEvents';
 
-test('A policy change banning a user on a directly watched list', function () {
+test('A policy change banning a user on a directly watched list', async function () {
+  const spammerToBanUserID = `@spam:example.com` as StringUserID;
+  const policyRoom = randomRoomID([]);
+  const { protectedRoomsSet, roomStateManager, policyRoomManager } =
+    await describeProtectedRoomsSet({
+      rooms: [
+        {
+          membershipDescriptions: [
+            {
+              sender: spammerToBanUserID,
+              membership: Membership.Join,
+            },
+          ],
+        },
+      ],
+      lists: [
+        {
+          room: policyRoom,
+        },
+      ],
+    });
+
   const description = findProtection('MemberBanSynchronisationProtection');
   if (description === undefined) {
     throw new TypeError(
@@ -12,13 +39,44 @@ test('A policy change banning a user on a directly watched list', function () {
     );
   }
   const consequenceProvider = createMock<BasicConsequenceProvider>();
-  const protectedRoomsSet = createMock<ProtectedRoomsSet>();
-  const protection = description.factory(
+  const consequenceSpy = jest.spyOn(
+    consequenceProvider,
+    'consequenceForUsersInRevision'
+  );
+  const protectionResult = description.factory(
     description,
     consequenceProvider,
     protectedRoomsSet,
     undefined,
     {}
   );
-  console.log(protection);
+  if (isError(protectionResult)) {
+    throw new TypeError('Should be able to construct the protection');
+  }
+  const protection = protectionResult.ok;
+
+  const policyRoomRevisionIssuer =
+    policyRoomManager.getFakePolicyRoomRevisionIssuer(policyRoom);
+  roomStateManager.appendState({
+    room: policyRoom,
+    policyDescriptions: [
+      { entity: spammerToBanUserID, type: PolicyRuleType.User },
+    ],
+  });
+  await waitForExpect(() => {
+    expect(policyRoomRevisionIssuer.getNumberOfRevisions()).toBe(1);
+  });
+  const revisionEntry = policyRoomRevisionIssuer.getLastRevision();
+  if (protection.handlePolicyChange === undefined) {
+    throw new TypeError(
+      `Protection should have the method to handle a policy change defined, is this test out of date?`
+    );
+  }
+  const protectionHandlerResult = await protection.handlePolicyChange.call(
+    protection,
+    revisionEntry[0],
+    revisionEntry[1]
+  );
+  expect(isOk(protectionHandlerResult)).toBeTruthy();
+  expect(consequenceSpy).toBeCalled();
 });
