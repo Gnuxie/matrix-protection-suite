0db9c3585bc1773d49c08649228f884601b10819
gnuxie <Gnuxie@protonmail.com>
2024-02-12 11:38:40 +0000

    Move Mjolnir attribution text to SPDX file attribution.

--- src/PolicyList/StandardPolicyListRevision.ts
+++ src/PolicyList/StandardPolicyListRevision.ts
@@ -2,9 +2,11 @@
 // Copyright 2019 - 2021 The Matrix.org Foundation C.I.C.
 //
 // SPDX-License-Identifier: AFL-3.0 AND Apache-2.0
-
+//
+// SPDX-FileAttributionText: <text>
 // This modified file incorporates work from mjolnir
 // https://github.com/matrix-org/mjolnir
+// </text>
 
 import { PolicyRuleType } from '../MatrixTypes/PolicyEvents';
 import { PolicyListRevision } from './PolicyListRevision';
--- src/Protection/ProtectedRoomsConfig/MjolnirProtectedRoomsEvent.ts
+++ src/Protection/ProtectedRoomsConfig/MjolnirProtectedRoomsEvent.ts
@@ -2,9 +2,11 @@
 // Copyright 2019 2022 The Matrix.org Foundation C.I.C.
 //
 // SPDX-License-Identifier: AFL-3.0 AND Apache-2.0
-
+//
+// SPDX-FileAttributionText: <text>
 // This modified file incorporates work from mjolnir
 // https://github.com/matrix-org/mjolnir
+// </text>
 
 import { StaticDecode, Type } from '@sinclair/typebox';
 import { Permalink } from '../../MatrixTypes/MatrixRoomReference';
