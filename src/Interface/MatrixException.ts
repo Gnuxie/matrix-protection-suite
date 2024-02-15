494a814afa53060332f2d648f0f4755026c7c64b
gnuxie <Gnuxie@protonmail.com>
2024-02-15 14:19:50 +0000

    Fix static method clash issue in MatrixException.

--- src/Interface/MatrixException.ts
+++ src/Interface/MatrixException.ts
@@ -5,14 +5,6 @@
 import { ActionError, ActionResult, ResultError } from './Action';
 import { ActionException, ActionExceptionKind } from './ActionException';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore 2417
-// https://github.com/microsoft/TypeScript/issues/4628
-// This is really a TypeScript L, no idea why they decided to make this decision
-// based on a subjective idea of intuition.
-// please remove the ts-ignore while editing the class that's best we can do
-// I can't be assed to try cookup some type gymnastics to get out of it
-// but feel free to try. @TS just fix it ffs.
 export class MatrixException extends ActionException implements ActionError {
   public constructor(
     exception: Error | unknown,
@@ -24,7 +16,11 @@ export class MatrixException extends ActionException implements ActionError {
     super(kind, exception, message);
   }
 
-  public static Result(options: {
+  /**
+   * Result wrapper, @see ActionError.
+   * Named because of https://github.com/microsoft/TypeScript/issues/4628.
+   */
+  public static R(options: {
     exception: Error;
     exceptionKind?: ActionExceptionKind;
     matrixErrorCode: string;
