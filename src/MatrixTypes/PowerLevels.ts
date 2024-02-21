7b4801f17faf48cbab414f7e292eda9088b6ef32
gnuxie <Gnuxie@protonmail.com>
2024-02-21 12:24:03 +0000

    Fix various issues with PowerLevel's event type.
    1. Content was optional, which is incorrect, needs to be union
       with empty content.
    2. Using `Type.Intersect` instead of `Type.Composite`
    3. Optional state_key and type
    4. Using `Static` instead of `StaticDecode`.

--- src/MatrixTypes/PowerLevels.ts
+++ src/MatrixTypes/PowerLevels.ts
@@ -9,82 +9,83 @@
 // https://github.com/matrix-org/matrix-spec
 // </text>
 
-import { Static, Type } from '@sinclair/typebox';
+import { StaticDecode, Type } from '@sinclair/typebox';
 import { StateEvent } from './Events';
 import { registerDefaultDecoder } from './EventDecoder';
 import { Value } from '../Interface/Value';
 
-export type PowerLevelsEventContent = Static<typeof PowerLevelsEventContent>;
-export const PowerLevelsEventContent = Type.Optional(
-  Type.Object({
-    ban: Type.Optional(
-      Type.Number({
-        description:
-          'The level required to ban a user. Defaults to 50 if unspecified.',
-      })
-    ),
-    events: Type.Optional(Type.Record(Type.String(), Type.Number())),
-    events_default: Type.Optional(
-      Type.Number({
-        description:
-          'The default level required to send message events. Can be\noverridden by the `events` key.  Defaults to 0 if unspecified.',
-      })
-    ),
-    invite: Type.Optional(
-      Type.Number({
-        description:
-          'The level required to invite a user. Defaults to 0 if unspecified.',
-      })
-    ),
-    kick: Type.Optional(
-      Type.Number({
-        description:
-          'The level required to kick a user. Defaults to 50 if unspecified.',
-      })
-    ),
-    redact: Type.Optional(
-      Type.Number({
-        description:
-          'The level required to redact an event sent by another user. Defaults to 50 if unspecified.',
-      })
-    ),
-    state_default: Type.Optional(
-      Type.Number({
-        description:
-          'The default level required to send state events. Can be overridden\nby the `events` key. Defaults to 50 if unspecified.',
-      })
-    ),
-    users: Type.Optional(Type.Record(Type.String(), Type.Number())),
-    users_default: Type.Optional(
-      Type.Number({
-        description:
-          'The power level for users in the room whose `user_id` is not mentioned in the `users` key. Defaults to 0 if\nunspecified.\n\n**Note**: When there is no `m.room.power_levels` event in the room, the room creator has\na power level of 100, and all other users have a power level of 0.     ',
-      })
-    ),
-    notifications: Type.Optional(
-      Type.Object(
-        {
-          room: Type.Optional(
-            Type.Number({
-              description:
-                'The level required to trigger an `@room` notification. Defaults to 50 if unspecified.',
-            })
-          ),
-        },
-        { additionalProperties: Type.Number() }
-      )
-    ),
-  })
-);
+export type PowerLevelsEventContent = StaticDecode<
+  typeof PowerLevelsEventContent
+>;
+export const PowerLevelsEventContent = Type.Object({
+  ban: Type.Optional(
+    Type.Number({
+      description:
+        'The level required to ban a user. Defaults to 50 if unspecified.',
+    })
+  ),
+  events: Type.Optional(Type.Record(Type.String(), Type.Number())),
+  events_default: Type.Optional(
+    Type.Number({
+      description:
+        'The default level required to send message events. Can be\noverridden by the `events` key.  Defaults to 0 if unspecified.',
+    })
+  ),
+  invite: Type.Optional(
+    Type.Number({
+      description:
+        'The level required to invite a user. Defaults to 0 if unspecified.',
+    })
+  ),
+  kick: Type.Optional(
+    Type.Number({
+      description:
+        'The level required to kick a user. Defaults to 50 if unspecified.',
+    })
+  ),
+  redact: Type.Optional(
+    Type.Number({
+      description:
+        'The level required to redact an event sent by another user. Defaults to 50 if unspecified.',
+    })
+  ),
+  state_default: Type.Optional(
+    Type.Number({
+      description:
+        'The default level required to send state events. Can be overridden\nby the `events` key. Defaults to 50 if unspecified.',
+    })
+  ),
+  users: Type.Optional(Type.Record(Type.String(), Type.Number())),
+  users_default: Type.Optional(
+    Type.Number({
+      description:
+        'The power level for users in the room whose `user_id` is not mentioned in the `users` key. Defaults to 0 if\nunspecified.\n\n**Note**: When there is no `m.room.power_levels` event in the room, the room creator has\na power level of 100, and all other users have a power level of 0.     ',
+    })
+  ),
+  notifications: Type.Optional(
+    Type.Object(
+      {
+        room: Type.Optional(
+          Type.Number({
+            description:
+              'The level required to trigger an `@room` notification. Defaults to 50 if unspecified.',
+          })
+        ),
+      },
+      { additionalProperties: Type.Number() }
+    )
+  ),
+});
 
-export type PowerLevelsEvent = Static<typeof PowerLevelsEvent>;
-export const PowerLevelsEvent = Type.Intersect([
-  StateEvent(PowerLevelsEventContent),
+export type PowerLevelsEvent = StaticDecode<typeof PowerLevelsEvent>;
+export const PowerLevelsEvent = Type.Composite([
+  Type.Omit(StateEvent(PowerLevelsEventContent), ['state_key', 'type']),
   Type.Object({
-    state_key: Type.Optional(
-      Type.String({ description: 'A zero-length string.', pattern: '^$' })
-    ),
-    type: Type.Optional(Type.Union([Type.Literal('m.room.power_levels')])),
+    state_key: Type.String({
+      description: 'A zero-length string.',
+      pattern: '^$',
+    }),
+    type: Type.Literal('m.room.power_levels'),
   }),
 ]);
 
