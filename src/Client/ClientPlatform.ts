d5e4a409536b40b7fffcc8c9024d2a68dba1b602
gnuxie <Gnuxie@protonmail.com>
2024-02-29 19:00:05 +0000

    Add RoomUnbanner to ClientPlatform.

--- src/Client/ClientPlatform.ts
+++ src/Client/ClientPlatform.ts
@@ -9,6 +9,7 @@ import { RoomJoiner } from './RoomJoiner';
 import { RoomKicker } from './RoomKicker';
 import { RoomResolver } from './RoomResolver';
 import { RoomStateEventSender } from './RoomStateEventSender';
+import { RoomUnbanner } from './RoomUnbanner';
 
 /**
  * A `ClientPlatform` has all the capabilities associated with a client.
@@ -29,4 +30,5 @@ export interface ClientPlatform {
   // TODO: Ideally we'd accept allowed state types here, so we can easily attenuate
   // which types can be sent.
   toRoomStateEventSender(): RoomStateEventSender;
+  toRoomUnbanner(): RoomUnbanner;
 }
