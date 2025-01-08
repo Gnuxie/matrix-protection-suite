985fe92a84ca95303b929054382411eb59ea3a67
gnuxie <Gnuxie@protonmail.com>
2025-01-08 14:38:31 +0000

    ServerACLEvent Schema was wrong.
    Content was marked as optional rather than each field being optional.
    
    discovered during investigation of
    https://github.com/the-draupnir-project/Draupnir/issues/499.

--- src/MatrixTypes/ServerACL.ts
+++ src/MatrixTypes/ServerACL.ts
@@ -15,28 +15,26 @@ import { Value } from '../Interface/Value';
 import { EDStatic } from '../Interface/Static';
 
 export type ServerACLContent = EDStatic<typeof ServerACLContent>;
-export const ServerACLContent = Type.Optional(
-  Type.Object({
-    allow_ip_literals: Type.Optional(
-      Type.Boolean({
-        description:
-          'True to allow server names that are IP address literals. False to\ndeny. Defaults to true if missing or otherwise not a boolean.\n\nThis is strongly recommended to be set to `false` as servers running\nwith IP literal names are strongly discouraged in order to require\nlegitimate homeservers to be backed by a valid registered domain name.',
-      })
-    ),
-    allow: Type.Optional(
-      Type.Array(Type.String(), {
-        description:
-          'The server names to allow in the room, excluding any port information.\nEach entry is interpreted as a [glob-style pattern](/appendices#glob-style-matching).\n\n**This defaults to an empty list when not provided, effectively disallowing\nevery server.**',
-      })
-    ),
-    deny: Type.Optional(
-      Type.Array(Type.String(), {
-        description:
-          'The server names to disallow in the room, excluding any port information.\nEach entry is interpreted as a [glob-style pattern](/appendices#glob-style-matching).\n\nThis defaults to an empty list when not provided.',
-      })
-    ),
-  })
-);
+export const ServerACLContent = Type.Object({
+  allow_ip_literals: Type.Optional(
+    Type.Boolean({
+      description:
+        'True to allow server names that are IP address literals. False to\ndeny. Defaults to true if missing or otherwise not a boolean.\n\nThis is strongly recommended to be set to `false` as servers running\nwith IP literal names are strongly discouraged in order to require\nlegitimate homeservers to be backed by a valid registered domain name.',
+    })
+  ),
+  allow: Type.Optional(
+    Type.Array(Type.String(), {
+      description:
+        'The server names to allow in the room, excluding any port information.\nEach entry is interpreted as a [glob-style pattern](/appendices#glob-style-matching).\n\n**This defaults to an empty list when not provided, effectively disallowing\nevery server.**',
+    })
+  ),
+  deny: Type.Optional(
+    Type.Array(Type.String(), {
+      description:
+        'The server names to disallow in the room, excluding any port information.\nEach entry is interpreted as a [glob-style pattern](/appendices#glob-style-matching).\n\nThis defaults to an empty list when not provided.',
+    })
+  ),
+});
 
 export type ServerACLEvent = EDStatic<typeof ServerACLEvent>;
 export const ServerACLEvent = Type.Intersect([
