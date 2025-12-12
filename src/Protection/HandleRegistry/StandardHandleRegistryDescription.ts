d575257d685044c4efb67f8ce664a9afaed372f5
gnuxie <Gnuxie@protonmail.com>
2025-12-12 12:39:33 +0000

    Missing copyright headers

--- src/Protection/HandleRegistry/StandardHandleRegistryDescription.ts
+++ src/Protection/HandleRegistry/StandardHandleRegistryDescription.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Gnuxie <Gnuxie@protonmail.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { Ok, isError } from '@gnuxie/typescript-result';
 import { AnyHandleDescription } from './HandleDescription';
 import { HandleRegistryDescription } from './HandleRegistryDescription';
--- src/Protection/ProtectionHandles.ts
+++ src/Protection/ProtectionHandles.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Gnuxie <Gnuxie@protonmail.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { Ok, Result, ResultError } from '@gnuxie/typescript-result';
 import { AllocatableLifetime } from '../Interface/Lifetime';
 import {
